2028b2e5adfadf334f06dae0572bd00ab2aec51f
CristianGomez05 <criscgb05@gmail.com>
2025-11-14 21:58:28 -0600

    Add: ejecutar createadmin en deploy

--- Frontend/panaderia-frontend/src/pages/Landing.jsx
+++ Frontend/panaderia-frontend/src/pages/Landing.jsx
@@ -1,4 +1,5 @@
 // src/pages/Landing.jsx
+import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
